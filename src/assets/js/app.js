49a86ef9245d5106a15c035f0894720233b4e707
andrei_labadaev <andrei-labadaev@mail.ru>
2021-11-07 14:58:47 +0300

    uppdate 14.58

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,15 @@
 //= components/datepicker.min.js
+//= components/jquery.timepicker.min.js
 
 //date
 $('.filter__menu-input').datepicker();
+$('.meet-date').datepicker();
+
+//date
+$('.meet-time').timepicker({
+    timeFormat: 'H:mm',
+    interval: 1,
+    minTime: '0',
+    maxTime: '23:59',
+    startTime: '00:00',
+});
